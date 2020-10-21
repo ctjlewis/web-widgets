620e1fe5baa6607a194491a1aec0ae7990890e5f
Christian Lewis <ctjl96@gmail.com>
2020-10-21 01:43:02 -0500

    WIP - freeze compiles, ~6% smaller

--- exports/exe.render.js
+++ exports/exe.render.js
@@ -7,4 +7,4 @@ import { debugLog } from '../lib/globals.js';
 import { default as page } from './exe.namespace.js';
 
 if (window['NO_RENDER']) debugLog('NO_RENDER flag detected - skipping render.');
-else page.render();
+else page.render(document.documentElement);
