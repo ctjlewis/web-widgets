27f024a025afa33d2b40ff8fa0181ec69d34dd2a
Christian Lewis <ctjl96@gmail.com>
2020-10-24 02:03:51 -0500

    Renaming flags, compiled stateful

--- exports/exe.namespace.js
+++ exports/exe.namespace.js
@@ -6,7 +6,16 @@
  * Closure Compiler.
  */
 
+/**
+ * Set a flag if the namespace is being compiled in freeze mode, to avoid
+ * serializing styles and other side effects that are not needed when thawing a
+ * frozen layout.
+ */
+// import { FROZEN } from '../lib/globals.js';
+// if (FROZEN) window.FROZEN_NAMESPACE = true;
+
+/**
+ * Export the page that we're rendering and all defined widgets.
+ */
 export { default } from '../lib/page.js';
 export * from './widgets.js';
-
-window.NAMESPACE_MODE = true;
